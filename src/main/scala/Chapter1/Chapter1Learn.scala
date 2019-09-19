01ae0c8919bcf9f375b6c21ed777d3c7898669df
plzero <pc6821pc@sina.com>
2019-09-19 08:20:46 +0800

    add a few code

--- src/main/scala/Chapter1/Chapter1Learn.scala
+++ src/main/scala/Chapter1/Chapter1Learn.scala
@@ -5,10 +5,26 @@ object Chapter1Learn{
 
   /**
    * 基础
+   * 基本类型：
+   * Byte
+   * Char
+   * Short
+   * Int
+   * Long
+   * Float
+   * Double
+   * Booean
+   *
    * @param args
    */
 
   def main(args: Array[String]): Unit = {
+    val values = "0Hello World!"
+
+    println(values.apply(1))
+    println(values(1))
+    println(values.asInstanceOf[String])
+    println(values(0).asInstanceOf[Int]-48)
 
   }
 }
