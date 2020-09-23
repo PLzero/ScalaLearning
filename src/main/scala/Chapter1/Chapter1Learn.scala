da99edc0248747ced08af38df3279de1fa5e1641
Payne-Cong <lee.pl2020@outlook.com>
2020-09-23 17:53:57 +0800

    add 词频统计

--- src/main/scala/Chapter1/Chapter1Learn.scala
+++ src/main/scala/Chapter1/Chapter1Learn.scala
@@ -3,6 +3,8 @@ package Chapter1
 object Chapter1Learn{
 
 
+
+
   /**
    * 基础
    * 基本类型：
@@ -19,12 +21,26 @@ object Chapter1Learn{
    */
 
   def main(args: Array[String]): Unit = {
-    val values = "0Hello World!"
+    val str = "A,B,C,A,A,D,C,B"
 
-    println(values.apply(1))
-    println(values(1))
-    println(values.asInstanceOf[String])
-    println(values(0).asInstanceOf[Int]-48)
+    val line =  str.split(",") //分割
+
+    val map_line = line.map(e=>(e,1)) // k => k,value
+
+    val group_line =  map_line.groupBy(e=>e._1) // 按Key分组 map_line.groupBy(_._1)
+
+    val result = group_line.map(e=>(e._1,e._2.size))
+
+    result.foreach(println)
+
+    val listLine = List("A B C","B C D","E C D","A C E")
+    val test =
+    listLine.flatMap(_.split(" "))
+      .map(_.trim)
+      .map((_,1))
+      .groupBy(_._1)
+      .map(e=>(e._1,e._2.size))
+
 
   }
 }
